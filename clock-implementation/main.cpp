ad880ab5ca8136acd0d3f45a4b339a483e2811cd
nakayan5 <tomoya.progress@gmail.com>
2022-12-22 21:19:12 +0900

    add

--- clock-implementation/main.cpp
+++ clock-implementation/main.cpp
@@ -11,6 +11,12 @@ using namespace std;
 //   int minute  分を表す   (0~59の値をとる)
 //   int second  秒を表す   (0~59の値をとる)
 
+struct Clock {
+  int hour; 
+  int minute;
+  int second; 
+
+
 // メンバ関数 set の定義を書く
 //   関数名: set
 //   引数: int h, int m, int s (それぞれ時、分、秒を表す)
@@ -18,6 +24,11 @@ using namespace std;
 //   関数の説明:
 //     時・分・秒を表す3つの引数を受け取り、
 //     それぞれ、メンバ変数 hour, minute, second に代入する
+  void set(int h, int m, int s) {
+    hour = h;
+    minute = m;
+    second = s;
+  };
 
 // メンバ関数 to_str の定義を書く
 //   関数名: to_str
@@ -28,6 +39,33 @@ using namespace std;
 //     時刻の文字列は次のフォーマット
 //     "HH:MM:SS"
 //     HH、MM、SSはそれぞれ時間、分、秒を2桁で表した文字列
+  string to_str() {
+    cout << "" << "" << endl;
+
+    string ret;
+ 
+    // 必要ではないが、書いておくと誤りがある場合に気づきやすくなる
+    if (!(0 <= hour && hour <= 23 &&
+          0 <= minute && minute <= 59 &&
+          0 <= second && second <= 59)) {
+      return "error";
+    }
+ 
+    if (hour < 10) ret += "0";
+    ret += to_string(hour);
+ 
+    ret += ":";
+ 
+    if (minute < 10) ret += "0";
+    ret += to_string(minute);
+ 
+    ret += ":";
+ 
+    if (second < 10) ret += "0";
+    ret += to_string(second);
+ 
+    return ret;
+  };
 
 // メンバ関数 shift の定義を書く
 //   関数名: shift
@@ -38,7 +76,44 @@ using namespace std;
 //     diff_second の値が負の場合、時刻を戻す
 //     diff_second の値が正の場合、時刻を進める
 //     diff_second の値は -86400 ~ 86400 の範囲を取とりうる
+  void shift(int diff_second) {
+    int diff_hour = diff_second / 3600; // intだから整数が入る（60分×60秒）
+    diff_second %= 3600;
+    int diff_minute = diff_second / 60;
+    diff_second %= 60;
+ 
+    second += diff_second;
+    if (second >= 60) {
+      // 分へ繰り上げ
+      minute += 1;
+      second -= 60;
+    } else if (second < 0) {
+      // 分から繰り下げ
+      minute -= 1;
+      second += 60;
+    }
+ 
+    minute += diff_minute;
+    if (minute >= 60) {
+      // 時へ繰り上げ
+      hour += 1;
+      minute -= 60;
+    } else if (minute < 0) {
+      // 時から繰り下げ
+      hour -= 1;
+      minute += 60;
+    }
+ 
+    hour += diff_hour;
+    if (hour >= 24) {
+      hour -= 24;
+    } else if (hour < 0) {
+      hour += 24;
+    }
+  
+  };
 
+};
 
 // -------------------
 // ここから先は変更しない
